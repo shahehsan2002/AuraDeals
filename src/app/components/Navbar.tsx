45cee04964ef64621d6d38d40d87a84efcbed5cb
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-19 23:41:09 +0600

    navbar created for mobile screen

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import Menu from "./Menu";
 
 const Navbar = () => {
   return (
-    <div className='h-20 md:px-8 lg:px-16 xl:px-32 2xl:px-64 '>Navbar</div>
-  )
-}
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative ">
+      {/* Mobile Navbar  */}
+      <div className="flex items-center justify-between h-full " >
+        <Link href={"/"}>
+        <div className="text-2xl tracking-wide" >AuraDeals</div>
+        </Link>
+        <Menu />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
